8b7935f342640c5bbddd9d8b1efacefde1844bad
Bryan <bryan@DEBIAN-LAPTOP.maarti.net>
2018-10-28 00:10:08 -0400

    Initial commit

---
+++ projects/checklist/src/lib/checklist.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { ChecklistComponent } from './checklist.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [ChecklistComponent],
+  exports: [ChecklistComponent]
+})
+export class ChecklistModule { }
